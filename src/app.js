67678419a5c8d571f26fb48a6224d3047b5bff6c
unknown <seba21salta@gmail.com>
2023-06-01 08:51:49 -0300

    res.header

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(morgan("dev"));
 app.use(express.json());
 
-/*  app.use((req, res, next)=>{
+  app.use((req, res, next)=>{
      res.header('Access-Control-Allow-Origin', '*');  // http://localhost:3000 (remplace por el *)
      res.header('Access-Control-Allow-Credentials', 'true');
      res.header(
@@ -23,7 +23,7 @@ app.use(express.json());
      );
      next();
 });                         //PUEDE SERVIR PARA EL DEPLOY (VER)
- */
+ 
 
 app.use( mainRouter);
 
